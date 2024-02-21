b760537e017434b474e01710f5baa22e05feb063
Susi <braeden8.susi6@gmail.com>
2024-02-21 14:33:32 -0500

    Movement complete!
    Created a base class for our game to share data (specifically the enum of Locations, and an UpdateLocation Function.
    
    Create the Input Asset, create movement with drag with crouching, jump function with grounded state, and camera movement with Sensitivity.

--- Assets/Scripts/Player/LocationManager.cs
+++ Assets/Scripts/Player/LocationManager.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LocationManager : MonoBehaviour
+public class LocationManager : DuckDuckGoose
 {
+    public Duck goose;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,12 @@ public class LocationManager : MonoBehaviour
     {
         
     }
+
+    override public void UpdateLocation(Location now)
+    {
+        myLoc = now;
+        goose.UpdateLocation(myLoc);
+        Debug.Log(myLoc);
+    }
+    
 }
